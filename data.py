9d5ff04a3a44847c74661c61279aa8a1d48b4801
santi <santiagoc333@gmail.com>
2024-02-27 21:27:15 -0700

    initial code, prints time domain data for all channels

---
+++ data.py
@@ -0,0 +1,24 @@
+# simple script for plotting time domain raw data from TDMS file
+
+import matplotlib.pyplot as plt
+from nptdms import TdmsFile
+import numpy as np
+import os # use for file navigation
+
+tdms_file = TdmsFile.read("10/12/Vibe_Continuous_230521_124306.tdms")
+
+channel_data = [] # array for storing the data for all channels
+for i in range(0,9):
+    channel_data.append(tdms_file["Time Domain"][f"Channel_0{i}"].data)
+
+num_samples = len(channel_data[0])
+time = np.linspace(0, num_samples - 1, num_samples)
+
+for i in range(0,9):
+    plt.plot(time, channel_data[i], label=f"Channel {i}")
+
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.title("Data from TDMS file")
+plt.legend()
+plt.show()
