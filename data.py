8cbb798e41cd969c3bae095597731edeb2407b89
santi <santiagoc333@gmail.com>
2024-02-28 18:08:22 -0700

    accessed time data, need to increment

--- data.py
+++ data.py
@@ -10,12 +10,18 @@ from scipy.signal import stft # STFT
 # moves up to parent folder '..' and opens the 'raw' folder for raw data
 tdms_path = '../raw/10/12/Vibe_Continuous_230521_124306.tdms'
 
+dt = 1/3000 # based on sampling rate
 
 tdms_file = TdmsFile.read(tdms_path)
+prop = tdms_file.properties
+start_time = prop['DateTime'] # getting the time in which data started being collected
+
+print(start_time)
 
 channel_data = [] # 2D array for storing the data for all channels
 
 for i in range(0,9):
+    
     channel_data.append(tdms_file["Time Domain"][f"Channel_0{i}"].data)
 
 num_samples = len(channel_data[0])
